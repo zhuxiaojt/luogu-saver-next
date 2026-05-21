d2e7bc162e66a38890ff861cf5e312f3cbccf9fa
Aak <anschluss_zeit@outlook.com>
2026-05-22 01:14:33 +0800

    fix(eslint): disable vue/no-v-html rule in ESLint configuration

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -31,6 +31,7 @@ export default [
             '@typescript-eslint/no-explicit-any': 'off',
             'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
             'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+            'vue/no-v-html': 'off',
             'vue/multi-word-component-names': 'off'
         }
     },
